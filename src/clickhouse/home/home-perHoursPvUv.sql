6882f87fd5722f36bd44f5015d923b14d5e3482b
ligang <ligang1@xsyxsc.com>
2021-07-20 18:21:25 +0800

    feat: 新增sql

--- src/clickhouse/home/home-perHoursPvUv.sql
+++ src/clickhouse/home/home-perHoursPvUv.sql
@@ -1,13 +1,29 @@
+/*
+home页面sql-app最近10小时每个小时的的uv和pv
+
+传入参数：
+1.appId
+2.startTime
+3.startTime
+4.day
+
+返回结果：
+1.appId
+2.pv
+3.uv
+4.statHour
+*/
+
 select appId,
        statHour,
-       sum(uv) as uv,
-       sum(pv) as pv
+       uv,
+       pv
 from (
          select appId,
                 uniqExact(guid)              as uv,
                 count(*)                     as pv,
-                SUBSTRING(insertTime, 1, 10) as statDate,
-                SUBSTRING(insertTime, 12, 2) as statHour
+                SUBSTRING(insertTime, 1, 10) as statDate,   /*截取日期*/
+                SUBSTRING(insertTime, 12, 2) as statHour    /*截取小时*/
          from default.flink_detail_ppo_all
          where partDate = '20210719'
          group by appId, SUBSTRING(insertTime, 1, 10), SUBSTRING(insertTime, 12, 2)
@@ -16,5 +32,4 @@ where statDate = '2021-07-19'
   and statHour >= '07'
   and statHour <= '16'
   and appId = 'wx6025c5470c3cb50c'
-group by appId, statHour
 order by statHour asc format JSON
---
+++ src/clickhouse/home/home-ppo.sql
@@ -0,0 +1,10 @@
+/*
+home页面sql-ppo查询结果
+
+传入参数：
+
+
+返回结果：
+urlCount:ppo表中url字段去重个数
+avgFpr:fpr平均时间
+*/
